5045b15b106fcd93c4f6dad7b73f25730e1530db
shubham.ralli <shubham.ralli@socialpilot.co>
2023-07-08 19:08:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="from">
+      <div className="fomr-title">
+        <h2>react popup</h2>
+      </div>
+
+      <div>name</div>
+      <input type="text" name="name"/>
+      <div>email</div>
+      <input type="email" name="name"/>
+      <div>phone</div>
+      <input type="text" name="name"/>
+     </div>
     </div>
   );
 }
